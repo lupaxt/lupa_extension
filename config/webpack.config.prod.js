8ab81c4fd2c7ab97177feb239fd51f642f928150
Markus Strasser <strasser.ms@gmail.com>
2018-08-19 11:41:10 +0200

    configure webpack.prod to not inject content script in popup index.html

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -247,6 +247,7 @@ module.exports = {
         new HtmlWebpackPlugin({
             inject: true,
             template: paths.appHtml,
+            chunks: ["app"],
             minify: {
                 removeComments: true,
                 collapseWhitespace: true,
@@ -260,6 +261,7 @@ module.exports = {
                 minifyURLs: true,
             },
         }),
+
         // Makes some environment variables available to the JS code, for example:
         // if (process.env.NODE_ENV === 'production') { ... }. See `./env.js`.
         // It is absolutely essential that NODE_ENV was set to production here.
