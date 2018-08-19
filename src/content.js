6b94ceffc0dfcd12ec73e4408792a048a34c2a77
Markus Strasser <strasser.ms@gmail.com>
2018-08-19 08:15:34 +0200

    setup and both popup and inject work

--- src/content.js
+++ src/content.js
@@ -1,3 +1,30 @@
 import "./content.css";
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+const container = document.getElementById('root');
+console.log(container, 'root');
+
+class Popper extends Component {
+    render() {
+        return (
+            <div>
+                <pre>HELLO </pre>
+            </div>
+        );
+    }
+}
+
+export {Popper}
 
 console.log('HHHEEEYoooo')
+
+injectApp()
+
+function injectApp() {
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("id", "markusExtend");
+    // newDiv.setAttribute("class", "wiz special swag");
+    document.body.appendChild(newDiv);
+    ReactDOM.render(<Popper/>, newDiv);
+}
+
---
+++ src/main.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Button from './components/Button';
+// import '../style.css'
+import StarRatingComponent from 'react-star-rating-component'
+// import firebase from 'firebase'
+
+
+//TODO abstract star component
+//TODO post and fetch from FiBa API
+//TODO emoji rating bar
+//TODO commenting on content or even text on a page -- eg kindle reader snippets
+//TODO add tooltip to Rating Component
+
+// Initialize Firebase
+//
+// const isFirebaseSetup = function() {
+//     if (!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
+//         window.alert('You have not configured and imported the Firebase SDK. ' +
+//             'Make sure you go through the codelab setup instructions and make ' +
+//             'sure you are running the codelab using `firebase serve`');
+//     }
+//     return false;
+// }
+
+
+class App extends React.Component {
+    constructor(props) {
+        super();
+
+        this.state = {
+            rating: 0,
+            rated: false,
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                Your App injected to DOM correctly!
+                <Button/>
+                <center style={{marginBottom: '40px', marginTop: '30px'}}>
+                    <Rate id={'rate123'} allowHalf value={3}/>
+                </center>
+                {console.log('teoreore', this.state)}
+                {this.state.rated ? (
+                    <React.Fragment>
+                    <textarea onChange={(ev) => console.log('val', ev.target.value)}
+                              placeholder="Now, how do you really feel?"
+                              cols="30" rows="5"/>
+                        <button>Comment ✅</button>
+                    </React.Fragment>
+
+                ) : null}
+                <StarRatingComponent
+                    name="rate1"
+                    starCount={5}
+                    editing={true}
+                    value={this.state.rating}
+                    onStarHover={(rating) => this.state.rated ? null : this.setState({rating: rating})}
+                    onStarClick={(rating) => this.setState(prev => ({rating, rated: true}))}
+                />
+                <button onClick={() => {
+                    this.setState({rated: false, rating: 0});
+                    //TODO API CALL TO DELETE RATING
+                }}>Remove rating
+                </button>
+            </div>
+        )
+    }
+}
+
+
+injectApp();
+
+
+function injectApp() {
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("id", "markusExtend");
+    // newDiv.setAttribute("class", "wiz special swag");
+    document.body.appendChild(newDiv);
+    ReactDOM.render(<App/>, newDiv);
+}
